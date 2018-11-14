330be877199f46e1ed46d95247dbfafb20c63029
shubham <shubhamtaneja1088@gmail.com>
2018-11-14 14:41:23 +0530

    Event Managememt System

---
+++ Event/src/java/login/check.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author dell
+ */
+public class check extends HttpServlet {
+    public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+    {
+         res.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        String id = req.getParameter("sname");
+        String fname=req.getParameter("semail");
+        String lname=req.getParameter("susername");
+        String pass = req.getParameter("spass");
+        
+         RecordCheck rs = new RecordCheck();
+        try {
+            rs.insert(id, fname, lname, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(check.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(check.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+}
